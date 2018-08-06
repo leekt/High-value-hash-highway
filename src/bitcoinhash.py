aab3fa292be5b137bb1fac57c79053c7d6787358
leekt216 <leekt216@gmail.com>
2018-08-06 14:27:31 +0900

    improved consistency by adding try - except

--- src/bitcoinhash.py
+++ src/bitcoinhash.py
@@ -13,9 +13,12 @@ def getHash(num):
     # set url for json response
     url = 'https://blockchain.info/block-height/%d?format=json' % num
     response = requests.get(url)
-    # get json data
-    json_data = json.loads(response.text)
-    return json_data['blocks'][0]['hash'], json_data['blocks'][0]['bits']
+    if response.ok :
+        # get json data
+        json_data = json.loads(response.text)
+        return json_data['blocks'][0]['hash'], json_data['blocks'][0]['bits']
+    else :
+        raise ValueError('response not ok.')
 
 f_ = open('./csv/hash_bits.csv','r')
 csv_ = csv.reader(f_)
@@ -36,7 +39,11 @@ if row_count <= 0:
 max_height = int(getBlockchainHeight())
 
 for i in range(int(row_count)-1,max_height):
-    h,b = getHash(i)
-    print("#"+str(i)+"\t\t"+str(h)+"\t"+str(b))
-    f.writerow([i,h,b])
+    try:
+        h,b = getHash(i)
+        print("#"+str(i)+"\t\t"+str(h)+"\t"+str(b))
+        f.writerow([i,h,b])
+    except ValueError as error:
+        print('Caught this error: ' + repr(error))
+        i = i-1
 
